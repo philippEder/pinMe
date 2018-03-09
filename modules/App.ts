020fc887f1f44eb72cd4eea1a6d9a513da50355c
PhilEder <p.eder92@gmx.at>
2018-03-10 00:39:31 +0100

    removed some copy paste comments

--- modules/App.ts
+++ modules/App.ts
@@ -3,32 +3,23 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import TodoRouter from './m_todo/routes/TodoRouter';
 
-// Creates and configures an ExpressJS web server.
 class App {
 
-  // ref to Express instance
   public express: express.Application;
 
-  //Run configuration methods on the Express instance.
   constructor() {
     this.express = express();
     this.middleware();
     this.routes();
   }
 
-  // Configure Express middleware.
   private middleware(): void {
     this.express.use(bodyParser.json());
     this.express.use(bodyParser.urlencoded({ extended: false }));
   }
 
-  // Configure API endpoints.
   private routes(): void {
-    /* This is just to get up and running, and to make sure what we've got is
-     * working so far. This function will change when we start to add more
-     * API endpoints */
     let router = express.Router();
-    // placeholder route handler
     router.get('/', (req, res, next) => {
       res.json({
         message: 'Hello World!'
