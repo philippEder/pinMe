792fa758db161e50c0f974195e56cd5ed688959e
PhilEder <p.eder92@gmx.at>
2018-03-10 00:46:23 +0100

    removed more comments

--- modules/m_todo/routes/TodoRouter.ts
+++ modules/m_todo/routes/TodoRouter.ts
@@ -9,25 +9,17 @@ export class TodoRouter {
     this.init();
   }
 
-  /**
-   * GET all Heroes.
-   */
   public getAll(req: Request, res: Response, next: NextFunction) {
     TodoDataAccess.getAllTodos();
     res.send('todosCalled');
   }
 
-  /**
-   * Take each handler, and attach to one of the Express.Router's
-   * endpoints.
-   */
   init() {
     this.router.get('/all', this.getAll);
   }
 
 }
 
-// Create the HeroRouter, and export its configured Express.Router
 const todoRouter = new TodoRouter();
 todoRouter.init();
 
