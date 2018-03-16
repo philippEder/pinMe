dab5c08064f024ee6ef24d04741cff79ff56f6f9
PhilEder <p.eder92@gmx.at>
2018-03-16 18:29:54 +0100

    added mock rest endpoint

--- modules/m_todo/routes/TodoRouter.ts
+++ modules/m_todo/routes/TodoRouter.ts
@@ -1,6 +1,7 @@
 import {Router, Request, Response, NextFunction} from 'express';
 import {TodoDataAccess} from '../service/TodoDataAccess';
 import { TodoService } from '../service/TodoService';
+import { Todo } from '../model/Todo';
 
 export class TodoRouter {
   router: Router
@@ -13,14 +14,15 @@ export class TodoRouter {
   init() {
     this.router.get('/all', this.getAll);
     this.router.get('/pending',this.getPending);
-    this.router.post("/insertTest", this.inserTestTodo);
+    this.router.post('/insertTest', this.inserTestTodo);
+    this.router.get("/mocked",this.getMockedData);
   }
 
-  public getById(req: Request, res : Response, nex: NextFunction) {
+  private getById(req: Request, res : Response, nex: NextFunction) {
     
   }
 
-  public getAll(req: Request, res: Response, next: NextFunction) {
+  private getAll(req: Request, res: Response, next: NextFunction) {
     TodoDataAccess.getAllTodos()
         .then(function (val) { 
           res.send(val); 
@@ -30,7 +32,7 @@ export class TodoRouter {
         })
   }
 
-  public inserTestTodo(erq: Request, res:Response, next : NextFunction) {
+  private inserTestTodo(erq: Request, res:Response, next : NextFunction) {
     TodoService.insertTestTodo()
         .then(function (val) { 
           res.send(val); 
@@ -40,10 +42,26 @@ export class TodoRouter {
         })
   }
 
-  public getPending(req : Request, res : Response, next : NextFunction) {
+  private getPending(req : Request, res : Response, next : NextFunction) {
     TodoDataAccess.getTodosOrderedByDueDateDescPaged(0,5)
                   .then((val) => res.send(val))
                   .catch((err) => res.send(err));
+  }
+
+  //lets keep this for some offline data
+  private getMockedData(req : Request, res : Response, next : NextFunction) {
+    res.send([
+      new Todo(1,'testName1','testDesc1',new Date(),new Date()),
+      new Todo(2,'testName2','testDesc2',new Date(),new Date()),
+      new Todo(3,'testName3','testDesc3',new Date(),new Date()),
+      new Todo(4,'testName4','testDesc4',new Date(),new Date()),
+      new Todo(5,'testName5','testDesc5',new Date(),new Date()),
+      new Todo(6,'testName6','testDesc6',new Date(),new Date()),
+      new Todo(7,'testName7','testDesc7',new Date(),new Date()),
+      new Todo(8,'testName8','testDesc8',new Date(),new Date()),
+      new Todo(9,'testName9','testDesc9',new Date(),new Date()),
+      new Todo(10,'testName10','testDesc10',new Date(),new Date())
+    ]);
   }
 
   
